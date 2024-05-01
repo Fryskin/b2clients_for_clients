b0c469c7443e31db45c03309e718850fff329b6b
fryskin <gman247@mail.ru>
2024-05-01 13:35:43 +0200

    Wrapped main.dart file

---
+++ lib/business_to_clients_for_clients_app.dart
@@ -0,0 +1,17 @@
+import 'package:b2clients_for_clients/router/router.dart';
+import 'package:flutter/material.dart';
+
+class BusinessToClientsForClientsApp extends StatelessWidget {
+  const BusinessToClientsForClientsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Business To Clients',
+
+      routes: routes,
+      // initialRoute: '',
+      // home: const MainPage(),
+    );
+  }
+}
